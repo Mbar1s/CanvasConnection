f94269b35d661ca2bdc3dc0abbe8ca52b36d2c2e
Mustafa Barış İmdat <108489416+Mbar1s@users.noreply.github.com>
2023-03-09 01:08:14 +0300

    Update Home.tsx

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,7 +12,7 @@ export default function Home() {
 
   useEffect(() => {
     axios
-      .get(`https://api.artic.edu/api/v1/artworks/129841`)
+      .get(`https://api.artic.edu/api/v1/artworks/27993`)
       .then((res) => {
         console.log(res);
         setArt(res.data.data);
@@ -25,16 +25,16 @@ export default function Home() {
   return (
     <div className="grow">
       <article className="bg-black text-white h-screen grid grid-cols-3 grid-rows-3">
-        <div className="h-full border col-span-2 row-span-2 border-white">
+        <div className="h-full border col-span-2 row-span-2 border-white flex items-center justify-center">
           <img
             className="h-full w-full"
             src={`https://www.artic.edu/iiif/2/${art?.image_id}/full/843,/0/default.jpg`}
           />
         </div>
-        <div className="h-full border row-span-2 border-white">
+        <div className="h-full border row-span-2 border-white flex items-center justify-center">
           <p>{art?.thumbnail.alt_text}</p>
         </div>
-        <div className="h-full border col-span-2 border-white">
+        <div className="h-full border col-span-2 border-white flex gap-4 items-center justify-center">
           <h1>{art?.title}</h1>
           <h1>{art?.artist_title}</h1>
         </div>
